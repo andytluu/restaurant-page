75ef1eaa23fe1ea368eb0b1d6b0e024ed79bfc72
Andy Luue <155850018+andytluu@users.noreply.github.com>
2025-01-26 16:48:24 -0800

    Added template.html, html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 // webpack.config.js
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
@@ -9,4 +9,10 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };
