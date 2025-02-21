daeb31823bf0f1e9ae7af295f40860ee88d84590
Andy Luue <155850018+andytluu@users.noreply.github.com>
2025-02-21 09:44:21 -0800

    Added tab switching logic to buttons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import home from "./home.js";
 import menu from "./menu.js";
 import contact from "./contact.js";
-//home();
+home();
 //menu();
-contact();
+//contact();
+
+const homeBtn = document.querySelector("#home-btn");
+const menuBtn = document.querySelector("#menu-btn");
+const contactBtn = document.querySelector("#contact-btn");
+const content = document.querySelector("#content");
+
+homeBtn.addEventListener("click",()=>{
+    content.innerHTML = "";
+    home();
+});
+
+menuBtn.addEventListener("click",()=>{
+    content.innerHTML = "";
+    menu();
+
+});
+
+contactBtn.addEventListener("click",()=>{
+    content.innerHTML = "";
+    contact();
+});
 console.log("Hjello world");
